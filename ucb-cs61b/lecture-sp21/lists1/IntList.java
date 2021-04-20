dd2cf0886b821719c94ea33fe13669949d82107e
Yixuan Rylee Li <ryleeliyixuan@gmail.com>
2021-04-20 12:15:30 +0800

    Lecture 4: primitive type and reference type + exercise on linked list

--- ucb-cs61b/lecture-sp21/lists1/IntList.java
+++ ucb-cs61b/lecture-sp21/lists1/IntList.java
@@ -34,11 +34,53 @@ public class IntList {
 		return rest.get(i - 1);
 	}
 
-	public static void main(String[] args) {
-		IntList L = new IntList(15, null);
-		L = new IntList(10, L);
-		L = new IntList(5, L);
+	/** Returns an IntList identical to L, but with
+	 * each element incremented by x. L is not allowed
+	 * to change. */
+	public static IntList incrList(IntList L, int x) {
+		/* Your code here. */
+		IntList Q = new IntList(L.first + x, null);
+		IntList head = Q;
+		while(L.rest != null){
+			head.rest = new IntList(L.rest.first + x, null);
+			head = head.rest;
+			L = L.rest;
+		}
+		return Q;
+	}
 
-		System.out.println(L.get(100));
+	/** Returns an IntList identical to L, but with
+	 * each element incremented by x. Not allowed to use
+	 * the 'new' keyword. */
+	public static IntList dincrList(IntList L, int x) {
+		/* Your code here. */
+		IntList Q = L;
+		IntList head = L;
+		while (head != null) {
+			head.first += 3;
+			head = head.rest;
+		}
+		return L;
+	}
+
+	public static void main(String[] args) {
+//		IntList L = new IntList(15, null);
+//		L = new IntList(10, L);
+//		L = new IntList(5, L);
+//
+//		System.out.println(L.get(2));
+
+		IntList L = new IntList(5, null);
+		L.rest = new IntList(7, null);
+		L.rest.rest = new IntList(9, null);
+
+//        System.out.println(L.size());
+//        System.out.println(L.iterativeSize());
+
+		// Test your answers by uncommenting. Or copy and paste the
+		// code for incrList and dincrList into IntList.java and
+		// run it in the visualizer.
+		System.out.println(incrList(L, 3));
+//		System.out.println(dincrList(L, 3));
 	}
 } 
